f5614546ce716c540fdaec184d6e41b7960cce2a
Maksym Solomko <max.exc7@gmail.com>
2022-12-24 16:11:37 +0200

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,4 @@
-import { FcSearch } from "react-icons/fc";
+// import { FcSearch } from "react-icons/fc";
 import PropTypes from 'prop-types';
 import {
   SearchBarHeader,
@@ -13,7 +13,8 @@ export const Searchbar = ({ onSubmit  }) => {
     <SearchBarHeader className="searchbar">
       <SearchForm onSubmit={onSubmit }>
         <SearchFormBtn type="submit" >                 
-            <FcSearch style={{ width: 24, height: 24 }} /> Search                  
+            {/* <FcSearch style={{ width: 24, height: 24 }} />  */}
+            Search                  
         </SearchFormBtn>
 
         <SearchFormInput          
